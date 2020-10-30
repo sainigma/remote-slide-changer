bb27c1c50a102c4eaa5ef639161175700394a195
sainigma <sainigma@gmail.com>
2020-10-30 18:50:54 +0200

    host registration now functional

--- host/main.py
+++ host/main.py
@@ -2,10 +2,50 @@
 
 import asyncio
 import websockets
+import json
+uri = 'ws://localhost:2048'
+config = {}
 
-async def test():
-    uri = 'ws://localhost:2048'
-    async with websockets.connect(uri) as websocket:
-        await websocket.send("hello world")
+async def register():
+  async with websockets.connect(uri) as websocket:
+      transmission = {}
+      transmission['role']='host'
+      if( config['id'] == '' ):
+        transmission['type']='newid'
+        await websocket.send(json.dumps(transmission))
+        response = await websocket.recv()
+        if( response ):
+          config['id'] = response
+          config['active'] = True
+      else:
+        transmission['type']='register'
+        transmission['id']=config['id']
+        await websocket.send(json.dumps(transmission))
+        response = await websocket.recv()
+        if( response == 'registered' ):
+          print('registration ok')
+          config['active'] = True
+      if(config['active']):
+          await listen(websocket)
+      else:
+        print('failed to register connection')
 
-asyncio.get_event_loop().run_until_complete(test())
+def init():
+  config['type'] = 'host'
+  config['id'] = 'abcdefg'
+  config['active'] = False
+  try:
+    asyncio.get_event_loop().run_until_complete(register())
+  except:
+    print('failed to connect to server')
+
+async def listen(websocket):
+  print('listening')
+  while(True):
+    response = await websocket.recv()
+    if( response ):
+      print( response )
+    else:
+      print('null')
+      
+init()
--- server/sockets.js
+++ server/sockets.js
@@ -2,9 +2,40 @@ const WebSocket = require('ws')
 const app = require('express')
 let config
 
+const hostActions = (socket, data) => {
+  switch(data.type){
+    case 'register':
+      if( data.id !== undefined ){
+        socket.send('registered')
+      }
+      break
+    case 'newid':
+      socket.send('abcdefg')
+      break
+  }
+}
+
+const clientActions = (socket, data) => {
+}
+
 const initSocket = (socket) => {
-  console.log(Object.keys(socket))
-  console.log(socket.protocol)
+  socket.on('message', (data)=>{
+    if( data !== undefined ){
+      data = JSON.parse(data)
+      console.log(data)
+      if( data.role !== undefined ){
+        if( data.role === 'host' ){
+          hostActions(socket, data)
+        }else if( data.role === 'client' ){
+          clientActions(socket, data)
+        }else{
+          socket.send('unhandled connection')
+        }
+      }else{
+        socket.send('unhandled connection')
+      }
+    }
+  })
 }
 
 const init = (httpsServer, cfg) => {
